ee92b2bda896cbcb4b0fdc99d4419eac11fe272b
ANGEL GABRILE VIVEROS RUBIO <gabrielviveros1909@gmail.com>
2026-01-16 17:10:22 -0600

    Corrige clase Auto para eliminar impresión y devolver información
    Se elimina la impresión en consola de la clase Auto y se agrega un método
    que devuelve la información del vehículo en formato de texto, respetando
    la separación entre modelo e interfaz de usuario.

--- java/src/edu/angel/viveros/actividad1poo/models/Auto.java
+++ java/src/edu/angel/viveros/actividad1poo/models/Auto.java
@@ -1,35 +1,35 @@
 package edu.angel.viveros.actividad1poo.models;
 
-
+/**
+ * Clase Auto.
+ * Representa un vehículo disponible para la venta.
+ * Contiene la información básica del automóvil.
+ */
 public class Auto {
-
-    /**
-     * Clase Auto.
-     * Representa un vehículo disponible para la venta.
-     * Contiene la información básica del automóvil.
-     */
 
     public String marca;
     public String modelo;
     public double precio;
 
-
     /**
      * Constructor de la clase Auto.
      *
-     * @param marca  marca del vehículo
+     * @param marca marca del vehículo
      * @param modelo modelo del vehículo
      * @param precio precio del vehículo
      */
-
     public Auto(String marca, String modelo, double precio) {
         this.marca = marca;
         this.modelo = modelo;
         this.precio = precio;
     }
 
-
-    public void mostrarInfo() {
-        System.out.println(marca + " " + modelo + " - $" + precio);
+    /**
+     * Devuelve la información del vehículo en formato de texto.
+     *
+     * @return información del auto
+     */
+    public String getInfo() {
+        return marca + " " + modelo + " - $" + precio;
     }
 }

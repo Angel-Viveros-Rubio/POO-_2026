9cd8f63637b1e971558ec94714459b8aadcb27c9
ANGEL GABRILE VIVEROS RUBIO <gabrielviveros1909@gmail.com>
2026-01-16 17:12:53 -0600

    Mantiene Starter como punto único de entrada de la aplicación
    Se conserva la clase Starter como el único punto de inicio del programa,
    limitando su función a ejecutar la interfaz de línea de comandos.

--- java/src/edu/angel/viveros/actividad1poo/starter/Starter.java
+++ java/src/edu/angel/viveros/actividad1poo/starter/Starter.java
@@ -1,19 +1,16 @@
 package edu.angel.viveros.actividad1poo.starter;
 
+import edu.angel.viveros.actividad1poo.ui.CLI;
+
 /**
  * Clase Starter.
  * Es el punto de entrada de la aplicación.
  * Su única función es iniciar la interfaz de línea de comandos.
  */
-
-import edu.angel.viveros.actividad1poo.ui.CLI;
-
-
 public class Starter {
 
     /**
      * Método principal de la aplicación.
-     * Inicia la ejecución del programa.
      *
      * @param args argumentos de línea de comandos
      */

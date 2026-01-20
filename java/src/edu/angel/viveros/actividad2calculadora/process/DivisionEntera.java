ea08544c21f3606748bc47e5223ab463aefe499b
ANGEL GABRILE VIVEROS RUBIO <gabrielviveros1909@gmail.com>
2026-01-19 20:12:23 -0600

    Corrección de la lógica de división
    Se corrigió la división para que funcione bien
    cuando el primer número es menor que el segundo.

--- java/src/edu/angel/viveros/actividad2calculadora/process/DivisionEntera.java
+++ java/src/edu/angel/viveros/actividad2calculadora/process/DivisionEntera.java
@@ -2,19 +2,19 @@ package edu.angel.viveros.actividad2calculadora.process;
 
 /**
  * Clase DivisionEntera.
- * Realiza división sin usar el operador /.
+ * Realiza división entera usando restas.
  */
 public class DivisionEntera {
 
     public int calcular(int a, int b) {
+        if (b == 0) {
+            return 0;
+        }
+
         Resta resta = new Resta();
         int contador = 0;
 
-        do {
+        while (a >= b) {
             a = (int) resta.calcular(a, b);
             contador++;
-        } while (a >= b);
-
-        return contador;
-    }
-}
+        }

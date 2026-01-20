265148afa500aebc58502df859e55ba9da71e3cc
ANGEL GABRILE VIVEROS RUBIO <gabrielviveros1909@gmail.com>
2026-01-19 20:09:39 -0600

    Corrección del cálculo del módulo
    Se corrigió el método del módulo para que el resultado
    sea el correcto y no quede vacío.

--- java/src/edu/angel/viveros/actividad2calculadora/process/Modulo.java
+++ java/src/edu/angel/viveros/actividad2calculadora/process/Modulo.java
@@ -2,17 +2,36 @@ package edu.angel.viveros.actividad2calculadora.process;
 
 /**
  * Clase Modulo.
- * Obtiene el residuo usando restas.
+ * Calcula el residuo de una división sin usar el operador %.
+ * Se apoya en la clase Resta.
  */
 public class Modulo {
 
+    /**
+     * Calcula el módulo entre dos números enteros.
+     *
+     * @param a dividendo
+     * @param b divisor
+     * @return residuo de la división
+     */
     public int calcular(int a, int b) {
+
+        if (b == 0) {
+            System.out.println("No se puede calcular módulo entre 0");
+            return 0;
+        }
+
         Resta resta = new Resta();
+        int resultado = a;
 
+        // Mientras el resultado siga siendo mayor o igual al divisor
         do {
-            a = (int) resta.calcular(a, b);
-        } while (a >= b);
+            if (resultado < b) {
+                break;
+            }
+            resultado = (int) resta.calcular(resultado, b);
+        } while (resultado >= b);
 
-        return a;
+        return resultado;
     }
 }
